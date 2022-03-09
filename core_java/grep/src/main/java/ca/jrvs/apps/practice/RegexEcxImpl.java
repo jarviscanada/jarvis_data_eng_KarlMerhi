caacd9958e05c92e84fc481a6ea3cdf8a26cf3c9
Karl Merhi <karlmerhi99@gmail.com>
2022-03-09 19:17:04 +0000

    commit

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexEcxImpl.java
+++
@@ -1,2 +0,0 @@
-package grep.src.main.java.ca.jrvs.apps.practice;public class RegEcxImpl {
-}
--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
+++
@@ -1,9 +0,0 @@
-package grep.src.main.java.ca.jrvs.apps.practice;
-
-public interface RegexExc {
-    public boolean matchJpeg(String filename);
-
-    public boolean match(String ip);
-
-    public boolean isEmptyLine(String line);
-}
