cef9a281d326d91f10cedc94b2f009a2d08f30fa
Karl Merhi <karlmerhi99@gmail.com>
2022-03-09 14:19:28 -0500

    Update RegexExec.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExec.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExec.java
@@ -1,2 +1,25 @@
-package ca.jrvs.apps.practice;public class RegexExec {
+package ca.jrvs.apps.practice;
+
+public interface RegexExec {
+    /**
+     * return true if filename extensions is jpg ot jpeg (case insensitive)
+     * @param filename
+     * @return
+     */
+    public boolean matchJpeg(String filename);
+
+    /**
+     * return true if ip is valid
+     * to simplify the problem, IP address range is from 0.0.0.0 to 999.999.999.999
+     * @param ip
+     * @return
+     */
+    public boolean match(String ip);
+
+    /**
+     * return true if line is empty (e.g. empty, white space, tabs, etc...)
+     * @param line
+     * @return
+     */
+    public boolean isEmptyLine(String line);
 }
