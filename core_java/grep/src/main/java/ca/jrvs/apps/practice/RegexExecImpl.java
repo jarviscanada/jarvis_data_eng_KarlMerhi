f9a1590151fa19fd05bc0f210bf901a0272280d5
Karl Merhi <karlmerhi99@gmail.com>
2022-03-09 14:19:12 -0500

    Update RegexExecImpl.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExecImpl.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExecImpl.java
@@ -1,2 +1,25 @@
-package ca.jrvs.apps.practice;public class RegexExecImpl {
+package ca.jrvs.apps.practice;
+
+public class RegexExecImpl implements RegexExec{
+    @Override
+    public boolean matchJpeg(String filename) {
+        String pattern = ".*\\.jpe?g$";
+        String normFilename = filename.toLowerCase(); // Normalize filename.
+
+        return normFilename.matches(pattern);
+    }
+
+    @Override
+    public boolean match(String ip) {
+        String pattern = "^(\\d{1,3}\\.){3}(\\d{1,3})$";
+
+        return ip.matches(pattern);
+    }
+
+    @Override
+    public boolean isEmptyLine(String line) {
+        String pattern = "\\s*";
+
+        return line.matches(pattern);
+    }
 }
