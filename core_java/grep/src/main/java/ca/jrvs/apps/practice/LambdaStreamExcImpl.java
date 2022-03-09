fec690fdd991234f8c607c31fc0b8ba7d7a2305c
Karl Merhi <karlmerhi99@gmail.com>
2022-03-09 14:19:43 -0500

    Update LambdaStreamExcImpl.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImpl.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImpl.java
@@ -1,2 +1,85 @@
-package ca.jrvs.apps.practice;public class LambdaStreamExcImpl {
+package ca.jrvs.apps.practice;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class LambdaStreamExcImpl implements LambdaStreamExc{
+
+    @Override
+    public Stream<String> createStrStream(String... strings) {
+        return Stream.of(strings);
+    }
+
+    @Override
+    public Stream<String> toUpperCase(String... strings) {
+        return Stream.of(strings).map(String::toUpperCase);
+    }
+
+    @Override
+    public Stream<String> filter(Stream<String> stringStream, String pattern) {
+        return stringStream.filter(str->(str.contains(pattern)));
+    }
+
+    @Override
+    public IntStream createIntStream(int[] arr) {
+        return IntStream.of(arr);
+    }
+
+    @Override
+    public <E> List<E> toList(Stream<E> stream) {
+        return stream.collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Integer> toList(IntStream intStream) {
+        return intStream.boxed().collect(Collectors.toList());
+    }
+
+    @Override
+    public IntStream createIntStream(int start, int end) {
+        return IntStream.range(start, end);
+    }
+
+    @Override
+    public DoubleStream squareRootIntStream(IntStream intStream) {
+        return intStream.asDoubleStream().map(Math::sqrt);
+    }
+
+    @Override
+    public IntStream getOdd(IntStream intStream) {
+        return intStream.filter(n -> n % 2 == 1);
+    }
+
+    @Override
+    public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
+        return (value)-> System.out.println(prefix+" "+value+" "+suffix);
+    }
+
+    @Override
+    public void printMessages(String[] messages, Consumer<String> printer) {
+        for(String str : messages) {
+            printer.accept(str);
+        }
+    }
+
+    @Override
+    public void printOdd(IntStream intStream, Consumer<String> printer) {
+        intStream.filter(num -> num % 2 == 1).forEach(num -> printer.accept(String.valueOf(num)));
+    }
+
+    @Override
+    public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
+        return ints.flatMap(Collection::stream).map(num -> num * num);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 }
